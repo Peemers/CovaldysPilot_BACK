d6f7153e206c4918d42ede49130b9c431026abbd
Math Peeters <pmathieu.peeters@gmail.com>
2026-05-18 16:51:31 +0200

    [s0-04] Config Ef-core

---
+++ CovaldysPilot.Infrastructure/DataBase/Context/CovaldysPilotDbContext.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CovaldysPilot.Infrastructure.DataBase.Context;
+
+public class CovaldysPilotDbContext(DbContextOptions<CovaldysPilotDbContext> options) : DbContext(options);
