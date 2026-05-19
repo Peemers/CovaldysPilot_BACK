05def7eeebcec56701fba9affdf7724873736273
Math Peeters <pmathieu.peeters@gmail.com>
2026-05-19 18:56:36 +0200

    feat: RefreshTokenConfiguration.cs

--- CovaldysPilot.Infrastructure/DataBase/Configurations/UserConfiguration.cs
+++ CovaldysPilot.Infrastructure/DataBase/Configurations/UserConfiguration.cs
@@ -9,27 +9,27 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
   public void Configure(EntityTypeBuilder<User> builder)
   {
     builder.HasKey(u => u.Id);
-        
+
     builder.Property(u => u.Id)
       .ValueGeneratedOnAdd();
 
     builder.Property(u => u.Email)
       .IsRequired()
       .HasMaxLength(256);
-        
+
     builder.HasIndex(u => u.Email)
       .IsUnique(); //db : email unique
-        
+
     builder.Property(u => u.Pseudo)
       .IsRequired()
       .HasMaxLength(50);
-        
+
     builder.HasIndex(u => u.Pseudo)
       .IsUnique(); //db pseudo unique
-        
+
     builder.Property(u => u.PasswordHash)
       .IsRequired();
-        
+
     builder.Property(u => u.CreatedAt)
       .IsRequired()
       .ValueGeneratedOnAdd();
