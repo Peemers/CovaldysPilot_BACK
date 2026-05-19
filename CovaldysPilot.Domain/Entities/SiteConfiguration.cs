c77c4a119727484763c65dc505598f80023a7278
Math Peeters <pmathieu.peeters@gmail.com>
2026-05-19 15:23:06 +0200

    feat: create SiteConfiguration.cs

---
+++ CovaldysPilot.Domain/Entities/SiteConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace CovaldysPilot.Domain.Entities;
+
+public class SiteConfiguration
+{
+  public bool IsMaintenanceMode { get; set; } = false;
+  public string? GlobalAlertMessage { get; set; }
+}
