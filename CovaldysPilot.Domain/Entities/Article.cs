9a800b43219d308c1b45ca3e78cd179226b9ae1b
Math Peeters <pmathieu.peeters@gmail.com>
2026-05-19 15:15:57 +0200

    feat: create Article.cs

---
+++ CovaldysPilot.Domain/Entities/Article.cs
@@ -0,0 +1,15 @@
+﻿namespace CovaldysPilot.Domain.Entities;
+
+public class Article : BaseEntity
+{
+  public required string Title { get; set; } = string.Empty;
+  public required string Content { get; set; } = string.Empty;
+  public string Author { get; set; } = string.Empty;
+  public int ViewCount { get; set; } = 0;
+  public DateTime PublicationDate { get; set; }
+
+  public required Guid UserId { get; set; }
+  public User User { get; set; } = null!;
+
+  public ICollection<ArticleImage> Images { get; set; } = new List<ArticleImage>();
+}
