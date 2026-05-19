7145ff75730be34058ff3ffae8c1fe6d2f4a99ab
Math Peeters <pmathieu.peeters@gmail.com>
2026-05-19 19:28:23 +0200

    feat: create EventConfiguration.cs

---
+++ CovaldysPilot.Infrastructure/DataBase/Configurations/EventConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using CovaldysPilot.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CovaldysPilot.Infrastructure.DataBase.Configurations;
+
+public class EventConfiguration : IEntityTypeConfiguration<Event>
+{
+  public void Configure(EntityTypeBuilder<Event> builder)
+  {
+    builder.HasKey(e => e.Id);
+
+    builder.Property(e => e.Id)
+      .ValueGeneratedOnAdd();
+
+    builder.Property(e => e.Name)
+      .IsRequired()
+      .HasMaxLength(200);
+
+    builder.Property(e => e.Description)
+      .IsRequired();
+
+    builder.Property(e => e.Location)
+      .HasMaxLength(300);
+
+    builder.Property(e => e.CoverImage)
+      .HasMaxLength(500);
+
+    builder.Property(e => e.StartDate)
+      .IsRequired();
+
+    builder.Property(e => e.EndDate)
+      .IsRequired();
+
+    builder.Property(e => e.RegistrationDeadline)
+      .IsRequired();
+
+    builder.Property(e => e.Status)
+      .IsRequired()
+      .HasConversion<string>();
+
+    builder.Property(e => e.CreatedAt)
+      .IsRequired()
+      .ValueGeneratedOnAdd();
+  }
+}
