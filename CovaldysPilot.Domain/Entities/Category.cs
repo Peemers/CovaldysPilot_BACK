a2026773eeff2acb0707484bad74836106103bdb
Math Peeters <pmathieu.peeters@gmail.com>
2026-05-19 14:50:25 +0200

    feat: create Category.cs

---
+++ CovaldysPilot.Domain/Entities/Category.cs
@@ -0,0 +1,7 @@
+﻿namespace CovaldysPilot.Domain.Entities;
+
+public class Category : BaseEntity
+{
+  public required string Name { get; set; }
+  public ICollection<Event> Events { get; set; } = new List<Event>();
+}
