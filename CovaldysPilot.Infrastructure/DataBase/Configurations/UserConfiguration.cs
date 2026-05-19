69ee2c2cb4abb1003a0f71e7cd5593765dd5d3d5
Math Peeters <pmathieu.peeters@gmail.com>
2026-05-19 16:12:56 +0200

    feat: create UserConfiguration.cs

---
+++ CovaldysPilot.Infrastructure/DataBase/Configurations/UserConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using CovaldysPilot.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CovaldysPilot.Infrastructure.DataBase.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+  public void Configure(EntityTypeBuilder<User> builder)
+  {
+    builder.HasKey(u => u.Id);
+        
+    builder.Property(u => u.Id)
+      .ValueGeneratedOnAdd();
+
+    builder.Property(u => u.Email)
+      .IsRequired()
+      .HasMaxLength(256);
+        
+    builder.HasIndex(u => u.Email)
+      .IsUnique(); //db : email unique
+        
+    builder.Property(u => u.Pseudo)
+      .IsRequired()
+      .HasMaxLength(50);
+        
+    builder.HasIndex(u => u.Pseudo)
+      .IsUnique(); //db pseudo unique
+        
+    builder.Property(u => u.PasswordHash)
+      .IsRequired();
+        
+    builder.Property(u => u.CreatedAt)
+      .IsRequired()
+      .ValueGeneratedOnAdd();
+  }
+}
