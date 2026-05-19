899582ee61e9093803b01377038d744c2a978eb6
Math Peeters <pmathieu.peeters@gmail.com>
2026-05-19 15:19:21 +0200

    feat: create ArticleImage.cs

---
+++ CovaldysPilot.Domain/Entities/ArticleImage.cs
@@ -0,0 +1,8 @@
+﻿namespace CovaldysPilot.Domain.Entities;
+
+public class ArticleImage : BaseEntity
+{
+  public required string Url { get; set; } = string.Empty;
+  public required Guid ArticleId { get; set; }
+  public Article Article { get; set; } = null!;
+}
