83f5e2d8c637651bfbc52c5b93f38da2e94e7c30
Math Peeters <pmathieu.peeters@gmail.com>
2026-05-19 15:06:31 +0200

    feat: create SignIn.cs

---
+++ CovaldysPilot.Domain/Entities/SignIn.cs
@@ -0,0 +1,15 @@
+﻿namespace CovaldysPilot.Domain.Entities;
+
+public class SignIn : BaseEntity
+{
+  public required Guid UserId { get; set; }
+  public User User { get; set; } = null!;
+
+  public required Guid EventId { get; set; }
+  public Event Event { get; set; } = null!;
+
+  public required DateTime RegistrationDate { get; set; }
+  public bool IsOnWaitingList { get; set; } = false;
+  public int? WaitingListPosition { get; set; }
+  public bool IsPaymentValid { get; set; } = false;
+}
