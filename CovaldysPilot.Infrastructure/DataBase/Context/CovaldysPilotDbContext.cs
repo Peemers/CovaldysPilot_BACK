74ec5944456b7d4e71e3c9a9c275027a4a670b06
Math Peeters <pmathieu.peeters@gmail.com>
2026-05-20 10:03:39 +0200

    feat: add dbset in CovaldysPilotDbContext.cs

--- CovaldysPilot.Infrastructure/DataBase/Context/CovaldysPilotDbContext.cs
+++ CovaldysPilot.Infrastructure/DataBase/Context/CovaldysPilotDbContext.cs
@@ -1,5 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CovaldysPilot.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace CovaldysPilot.Infrastructure.DataBase.Context;
 
-public class CovaldysPilotDbContext(DbContextOptions<CovaldysPilotDbContext> options) : DbContext(options);
+public class CovaldysPilotDbContext(DbContextOptions<CovaldysPilotDbContext> options) : DbContext(options)
+{
+  //syntaxe plus récente, plus sécuritaire car pas de setter, impossible d'écraser par erreur.
+  public DbSet<User> Users => Set<User>();
+  public DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
+  public DbSet<Event> Events => Set<Event>();
+  public DbSet<Category> Categories => Set<Category>();
+  public DbSet<EventCategory> EventCategories => Set<EventCategory>();
+  public DbSet<SignIn> SignIns => Set<SignIn>();
+  public DbSet<Review> Reviews => Set<Review>();
+  public DbSet<Article> Articles => Set<Article>();
+  public DbSet<ArticleImage> ArticleImages => Set<ArticleImage>();
+  public DbSet<SiteConfiguration> SiteConfigurations => Set<SiteConfiguration>();
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    base.OnModelCreating(modelBuilder);
+
+    // Applique toutes les configurations du dossier Configurations
+    modelBuilder.ApplyConfigurationsFromAssembly(typeof(CovaldysPilotDbContext).Assembly);
+  }
+}
