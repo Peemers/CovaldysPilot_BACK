0fe1aa03bfcc793503883631d3dca326a790fd25
Math Peeters <pmathieu.peeters@gmail.com>
2026-05-20 13:25:58 +0200

    feat: add DataAnnotations to :
    LoginRequestDto.cs
    RefreshTokenRequestDto.cs
    RegisterRequestDto.cs

--- CovaldysPilot.Application/DTOs/Auth/Request/LoginRequestDto.cs
+++ CovaldysPilot.Application/DTOs/Auth/Request/LoginRequestDto.cs
@@ -1,7 +1,14 @@
-﻿namespace CovaldysPilot.Application.DTOs.Auth.Request;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CovaldysPilot.Application.DTOs.Auth.Request;
 
 public class LoginRequestDto
 {
+  [Required(ErrorMessage = "L'email ou le pseudo est obligatoire.")]
   public required string EmailOrPseudo { get; set; }
+  
+  [Required(ErrorMessage = "Le mot de passe est obligatoire.")] 
   public required string Password { get; set; }
+  
+  //pas de contrainte de taille au cas ou un user avec un ancien mdp valide voudrait se connecter.
 }
--- CovaldysPilot.Application/DTOs/Auth/Request/RefreshTokenRequestDto.cs
+++ CovaldysPilot.Application/DTOs/Auth/Request/RefreshTokenRequestDto.cs
@@ -1,6 +1,9 @@
-﻿namespace CovaldysPilot.Application.DTOs.Auth.Request;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CovaldysPilot.Application.DTOs.Auth.Request;
 
 public class RefreshTokenRequestDto
 {
+  [Required(ErrorMessage = "Le refresh token est obligatoire.")]
   public required string RefreshToken { get; set; }
 }
