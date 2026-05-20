77d942501d2745757c345af009b18d38db272109
Math Peeters <pmathieu.peeters@gmail.com>
2026-05-20 09:45:51 +0200

    feat: create ArticleConfiguration.cs refactor: Guid UserId nullable in Article.cs

--- CovaldysPilot.Domain/Entities/Article.cs
+++ CovaldysPilot.Domain/Entities/Article.cs
@@ -7,8 +7,10 @@ public class Article : BaseEntity
   public string Author { get; set; } = string.Empty;
   public int ViewCount { get; set; } = 0;
   public DateTime PublicationDate { get; set; }
-
-  public required Guid UserId { get; set; }
+  
+  //nullable pour la suppression d'un user (si il a ecrit par exemple un article la fk devient nul mais l'article reste
+  public Guid? UserId { get; set; } 
+  
   public User User { get; set; } = null!;
 
   public ICollection<ArticleImage> Images { get; set; } = new List<ArticleImage>();
---
+++ CovaldysPilot.Infrastructure/DataBase/Configurations/ArticleConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using CovaldysPilot.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CovaldysPilot.Infrastructure.DataBase.Configurations;
+
+public class ArticleConfiguration : IEntityTypeConfiguration<Article>
+{
+  public void Configure(EntityTypeBuilder<Article> builder)
+  {
+    builder.HasKey(a => a.Id);
+
+    builder.Property(a => a.Id)
+      .ValueGeneratedOnAdd();
+
+    builder.Property(a => a.Title)
+      .IsRequired()
+      .HasMaxLength(300);
+
+    builder.Property(a => a.Content)
+      .IsRequired();
+
+    builder.Property(a => a.Author)
+      .HasMaxLength(100);
+
+    builder.Property(a => a.CreatedAt)
+      .IsRequired()
+      .ValueGeneratedOnAdd();
+
+    // Relation avec User (audit)
+    builder.HasOne(a => a.User)
+      .WithMany(u => u.Articles)
+      .HasForeignKey(a => a.UserId)
+      .OnDelete(DeleteBehavior.SetNull);
+  }
+}
