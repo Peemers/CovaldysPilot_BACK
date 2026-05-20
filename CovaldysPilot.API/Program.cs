56eb96032ebc86a2db332189a384aa27c628f164
Math Peeters <pmathieu.peeters@gmail.com>
2026-05-20 12:22:29 +0200

    feat: Clean Program.cs with Extension

--- CovaldysPilot.API/Program.cs
+++ CovaldysPilot.API/Program.cs
@@ -1,7 +1,6 @@
-using System.Threading.RateLimiting;
-using CovaldysPilot.Infrastructure.DataBase.Context;
-using Microsoft.AspNetCore.RateLimiting;
-using Microsoft.EntityFrameworkCore;
+using CovaldysPilot.API.Extensions;
+using CovaldysPilot.Application.Extensions;
+using CovaldysPilot.Infrastructure.Extensions;
 using Serilog;
 
 Log.Logger = new LoggerConfiguration()
@@ -14,31 +13,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog();
 builder.Services.AddOpenApi();
-builder.Services.AddDbContext<CovaldysPilotDbContext>(options =>
-  options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddRateLimiter(options =>
-{
-  // Politique globale : 30 requêtes par minute
-  options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
-    RateLimitPartition.GetFixedWindowLimiter(
-      partitionKey: context.Connection.RemoteIpAddress?.ToString() ?? "unknown",
-      factory: _ => new FixedWindowRateLimiterOptions
-      {
-        PermitLimit = 30,
-        Window = TimeSpan.FromMinutes(1)
-      }));
-
-  // Politique stricte pour l'authentification : 5 tentatives par 5 minutes
-  options.AddTokenBucketLimiter("auth", authOptions =>
-  {
-    authOptions.TokenLimit = 9;
-    authOptions.TokensPerPeriod = 3;
-    authOptions.ReplenishmentPeriod = TimeSpan.FromSeconds(20);
-  });
-
-  options.RejectionStatusCode = 429;
-});
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddRateLimiterPolicies();
 
 
 var app = builder.Build();
---
+++ CovaldysPilot.Infrastructure/Extensions/InfrastructureExtensions.cs
@@ -0,0 +1,29 @@
+﻿using CovaldysPilot.Application.Interfaces.Repositories;
+using CovaldysPilot.Application.Interfaces.Services;
+using CovaldysPilot.Infrastructure.DataBase.Context;
+using CovaldysPilot.Infrastructure.Repositories;
+using CovaldysPilot.Infrastructure.Security;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CovaldysPilot.Infrastructure.Extensions;
+
+public static class InfrastructureExtensions
+{
+  public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+  {
+    // DbContext
+    services.AddDbContext<CovaldysPilotDbContext>(options =>
+      options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+    // Repositories
+    services.AddScoped<IUserRepository, UserRepository>();
+    services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+
+    // Services
+    services.AddScoped<IJwtService, JwtService>();
+
+    return services;
+  }
+}
