a56622c1d35d488e2d3de8accd62f161f0fd2734
Math Peeters <pmathieu.peeters@gmail.com>
2026-05-20 09:13:42 +0200

    feat: create EventCategoryConfiguration.cs

---
+++ CovaldysPilot.Infrastructure/DataBase/Configurations/EventCategoryConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using CovaldysPilot.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CovaldysPilot.Infrastructure.DataBase.Configurations;
+
+public class EventCategoryConfiguration : IEntityTypeConfiguration<EventCategory>
+{
+  public void Configure(EntityTypeBuilder<EventCategory> builder)
+  {
+    // Clé primaire composite
+    builder.HasKey(ec => new { ec.EventId, ec.CategoryId });
+
+    // Relation avec Event
+    builder.HasOne(ec => ec.Event)
+      .WithMany(e => e.EventCategories)
+      .HasForeignKey(ec => ec.EventId)
+      .OnDelete(DeleteBehavior.Cascade);
+
+    // Relation avec Category
+    builder.HasOne(ec => ec.Category)
+      .WithMany(c => c.EventCategories)
+      .HasForeignKey(ec => ec.CategoryId)
+      .OnDelete(DeleteBehavior.Cascade);
+  }
+}
