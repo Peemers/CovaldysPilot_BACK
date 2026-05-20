d6cbdd8e2a08aece9952f79853ad51b747515502
Math Peeters <pmathieu.peeters@gmail.com>
2026-05-20 09:34:53 +0200

    feat: create ReviewConfiguration.cs

---
+++ CovaldysPilot.Infrastructure/DataBase/Configurations/ReviewConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using CovaldysPilot.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CovaldysPilot.Infrastructure.DataBase.Configurations;
+
+public class ReviewConfiguration : IEntityTypeConfiguration<Review>
+{
+  public void Configure(EntityTypeBuilder<Review> builder)
+  {
+    builder.HasKey(r => r.Id);
+
+    builder.Property(r => r.Id)
+      .ValueGeneratedOnAdd();
+
+    builder.Property(r => r.Note)
+      .IsRequired();
+
+    builder.Property(r => r.CreatedAt)
+      .IsRequired()
+      .ValueGeneratedOnAdd();
+
+    // Relation avec User
+    builder.HasOne(r => r.User)
+      .WithMany(u => u.Reviews)
+      .HasForeignKey(r => r.UserId)
+      .OnDelete(DeleteBehavior.Cascade);
+
+    // Relation avec Event
+    builder.HasOne(r => r.Event)
+      .WithMany(e => e.Reviews)
+      .HasForeignKey(r => r.EventId)
+      .OnDelete(DeleteBehavior.Restrict);
+  }
+}
