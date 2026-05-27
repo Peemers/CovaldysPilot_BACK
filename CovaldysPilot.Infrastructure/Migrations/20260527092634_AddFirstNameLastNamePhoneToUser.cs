943507e150476bb522cb2d679ae2eb731bd16006
Math Peeters <pmathieu.peeters@gmail.com>
2026-05-27 11:58:42 +0200

    [FIX] add  migration AddFirstNameLastNamePhoneToUser

---
+++ CovaldysPilot.Infrastructure/Migrations/20260527092634_AddFirstNameLastNamePhoneToUser.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CovaldysPilot.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFirstNameLastNamePhoneToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Users",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                columns: new[] { "FirstName", "LastName", "PhoneNumber" },
+                values: new object[] { "Anabelle", "AdminTest", "+32470.32.32.32" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Users");
+        }
+    }
+}
