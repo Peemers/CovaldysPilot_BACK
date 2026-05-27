d6cda7277393572606448251e8a5b634c79f93ab
Math Peeters <pmathieu.peeters@gmail.com>
2026-05-27 10:56:56 +0200

    feat: create IEventRepository.cs, IEventService.cs

---
+++ CovaldysPilot.Application/Interfaces/Services/IEventService.cs
@@ -0,0 +1,16 @@
+﻿using CovaldysPilot.Application.DTOs.Event.Request;
+using CovaldysPilot.Application.DTOs.Event.Response;
+
+namespace CovaldysPilot.Application.Interfaces.Services;
+
+public interface IEventService
+{
+  Task<IEnumerable<EventResponseDto>> GetAllAsync(Guid? currentUserId = null);
+  Task<EventResponseDto?> GetByIdAsync(Guid id, Guid? currentUserId = null);
+  Task<EventResponseDto> CreateAsync(CreateEventRequestDto dto);
+  Task<EventResponseDto> UpdateAsync(Guid id, UpdateEventRequestDto dto);
+  Task DeleteAsync(Guid id);
+  Task CancelAsync(Guid id);
+  Task StartAsync(Guid id);
+  Task CloseAsync(Guid id);
+}
