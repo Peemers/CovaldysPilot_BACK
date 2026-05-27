de00f24526334dd1ba729282c59533e493d87ab0
Math Peeters <pmathieu.peeters@gmail.com>
2026-05-27 15:44:58 +0200

    feat: add CreateSignInRequestDto.cs, ISignInRepository.cs, ISignInService.cs, SignInMapper.cs, SignInRepository.cs, SignInResponseDto.cs

---
+++ CovaldysPilot.Application/Mappers/SignInMapper.cs
@@ -0,0 +1,21 @@
+﻿using CovaldysPilot.Application.DTOs.SignIn.Response;
+using CovaldysPilot.Domain.Entities;
+
+namespace CovaldysPilot.Application.Mappers;
+
+public static class SignInMapper
+{
+  public static SignInResponseDto ToSignInResponseDto(this SignIn signIn)
+  {
+    return new SignInResponseDto
+    {
+      Id = signIn.Id,
+      EventId = signIn.EventId,
+      UserId = signIn.UserId,
+      RegistrationDate = signIn.RegistrationDate,
+      IsOnWaitingList = signIn.IsOnWaitingList,
+      WaitingListPosition = signIn.WaitingListPosition,
+      IsPaymentValid = signIn.IsPaymentValid
+    };
+  }
+}
