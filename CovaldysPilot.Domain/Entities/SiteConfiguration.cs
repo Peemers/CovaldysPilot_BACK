0159a4fe8a60d70b1492d3335ce01b0d6db1fa2d
Math Peeters <pmathieu.peeters@gmail.com>
2026-06-10 11:51:09 +0200

    fix : siteConfiguration default value

--- CovaldysPilot.Domain/Entities/SiteConfiguration.cs
+++ CovaldysPilot.Domain/Entities/SiteConfiguration.cs
@@ -2,7 +2,7 @@
 
 public class SiteConfiguration
 {
-  public int Id { get; set; } = 1;
+  public int Id { get; set; }
   public bool IsMaintenanceMode { get; set; } = false;
   public string? GlobalAlertMessage { get; set; }
 }
