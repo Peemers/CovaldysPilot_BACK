131bffee9ce07fc31673cf7cd61b508a9f4a04fa
Math Peeters <pmathieu.peeters@gmail.com>
2026-06-11 12:20:29 +0200

    feat: create add user by admin

---
+++ CovaldysPilot.Application/DTOs/User/Response/CreateUserManuallyResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CovaldysPilot.Application.DTOs.User.Response;
+
+public class CreateUserManuallyResponseDto
+{
+  public Guid Id { get; set; }
+  public required string Pseudo { get; set; }
+  public required string Email { get; set; }
+  public required string FirstName { get; set; }
+  public string? LastName { get; set; }
+  public bool IsMembershipUpToDate { get; set; }
+  //  UNE SEULE FOIS pour l'admin, jamais stock en clair en DB !!!!!
+  public required string TemporaryPassword { get; set; }
+}
